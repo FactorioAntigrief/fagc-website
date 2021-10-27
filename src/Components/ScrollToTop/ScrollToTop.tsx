88453997a0adba6d070818c72a5a46405db033f6
oof2win2 <honza.koco44@gmail.com>
2021-10-27 16:22:26 +0200

    :bug: Fix scroll to top button not working

--- src/Components/ScrollToTop/ScrollToTop.tsx
+++ src/Components/ScrollToTop/ScrollToTop.tsx
@@ -1,31 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import { Fab, IconButton, Zoom, Button } from '@mui/material'
-import {ExpandLess} from "@mui/icons-material"
-import "./ScrollToTop.css"
+import React, { useState } from "react"
+import { Fab, IconButton, Zoom, Button } from "@mui/material"
+import { ExpandLess } from "@mui/icons-material"
 
 const ScrollToTopComponent: React.FC = () => {
-	const [shown, setShown] = useState(true)
+	const [shown, setShown] = useState(false)
 
 	const handleScroll = () => {
-		setShown(window.screenY > 300)
+		setShown(window.scrollY > 100)
 	}
 
-	useEffect(() => {
-		window.addEventListener("scroll", handleScroll)
-		return window.removeEventListener("scroll", handleScroll)
-	}, [])
+	window.addEventListener("scroll", handleScroll)
 
 	return (
-		<Zoom
-			in={shown}
+		<Zoom in={shown}>
+			<Fab
+				sx={{
+					position: "fixed",
+					bottom: "50px",
+					right: "50px",
+				}}
 			>
-				<Fab className="scrollbutton">
-					<Button
-						onClick={() => window.scrollTo({top: 0, behavior: "smooth"})}
-					>
-						<IconButton><ExpandLess /></IconButton>
-					</Button>
-				</Fab>
+				<Button
+					// className="scrollbutton"
+					onClick={() =>
+						window.scrollTo({ top: 0, behavior: "smooth" })
+					}
+				>
+					<IconButton>
+						<ExpandLess />
+					</IconButton>
+				</Button>
+			</Fab>
 		</Zoom>
 	)
 }
